a11cc380d190bb4f816bda65d1e9544e18106450
Adam Mos <moztrom93@gmail.com>
2024-11-23 19:41:52 +0100

    wrote tests for part 3, fixed smaller issues, corrected bugs, should work now

--- Part3/DatesImproved.java
+++ Part3/DatesImproved.java
@@ -1,6 +1,9 @@
+//package Part3;
+
 import java.io.*;
 
 class DatesImproved {
+
 
   /* Precondition: month is between 1 and 12, inclusive */
   /* Postcondition: returns the number of days in the given month */
@@ -19,10 +22,15 @@ class DatesImproved {
   }
 
   private static int checkMonth(int month){
-    if (month > 0 && month <= 12) {
+    /**
+     * Changed month so indexing makes more sense (january = 1, feb = 2 and so forth).
+     */
+    int inputMonth = month;
+    month = month - 1;
+    if (month >= 0 && month <= 12) {
       return month;
     } else {
-      throw new IllegalArgumentException("Invalid month number: " + month);
+      throw new IllegalArgumentException("Invalid month number: " + inputMonth);
     }
   }
 
@@ -36,20 +44,24 @@ class DatesImproved {
   }
 
 
-  public static void main (String[] args) {
-    int someMonth, someDay;
-    int laterMonth, laterDay;
+  /**
+   * For testing Purposes
+   * @param m1
+   * @param d1
+   * @param m2
+   * @param d2
+   * @return
+   */
+  public static int calculateTimeBetween (int m1, int d1, int m2, int d2){
+
+
+    int someMonth = checkMonth(m1);
+    int someDay = checkDay(m1,d1);
+
+    int laterMonth = checkMonth(m2);
+    int laterDay = checkDay(m2,d2);
 
     int aMonth;
-    /**
-     * Changed month so indexing makes more sense (january = 1, feb = 2 and so forth).
-     */
-
-    someMonth = checkMonth(Integer.parseInt(args[0]));
-    someDay = checkDay(Integer.parseInt(args[1]), someMonth);
-
-    laterMonth = checkMonth(Integer.parseInt(args[2]) - 1);
-    laterDay = checkDay(Integer.parseInt(args[3]), laterMonth);
 
     /* Used to record what day in the year the first day  */
     /* of someMonth and laterMonth are. */
@@ -60,22 +72,48 @@ class DatesImproved {
       someDayInYear = someDayInYear + daysInMonth(aMonth);
     }
 
-
-
     /**
-     * FIXED BUG
+     * Fixed bug where loop did not reset aMonth to 0.
      */
     for (aMonth = 0; aMonth < laterMonth; aMonth = aMonth + 1) {
       laterDayInYear = laterDayInYear + daysInMonth(aMonth);
     }
 
-    /* The answer */
     int daysBetween = 0;
-    System.out.println("The difference in days between " +
-        someMonth + "/" + someDay + " and " +
-        laterMonth + "/" + laterDay + " is: ");
+
     daysBetween = laterDayInYear - someDayInYear;
     daysBetween = daysBetween + laterDay - someDay;
+
+    // Fixed a bug where negative values leaked through for when laterMonth < startMonth (or laterday < startDay)
+    if(daysBetween < 0) {daysBetween = 365 - (daysBetween * (-1));}
+
+
+    return daysBetween;
+  }
+
+
+  /**
+   * Changed Main structure, all calculation now made from calculateTimeBetween method.
+   * @param args
+   */
+  public static void main (String[] args) {
+
+    int daysBetween = 0;
+
+    int m1 = Integer.parseInt(args[0]);
+    int d1 = Integer.parseInt(args[1]);
+    int m2 = Integer.parseInt(args[2]);
+    int d2 = Integer.parseInt(args[3]);
+
+    daysBetween = calculateTimeBetween(m1, d1, m2, d2);
+
+    /* The answer */
+    System.out.println("The difference in days between " +
+        m1 + "/" + d1 + " and " +
+        m2 + "/" + d2 + " is: ");
     System.out.println(daysBetween);
   }
+
+
+
 }
--- Part3/TestDatesImproved.java
+++ Part3/TestDatesImproved.java
@@ -1,58 +1,97 @@
 package Part3;
+import static Part3.DatesImproved.calculateTimeBetween;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestDatesImproved
-{
+public class TestDatesImproved {
+
+
+
 
 
   /** VALID INPUTS **/
 
   @Test
   public void test_Day1_EQ_Day2(){
-
+    int result = calculateTimeBetween(1, 15, 4, 15);
+    Assertions.assertEquals(31, result);
   }
+
+  @Test
+  public void second_test_Day1_EQ_Day2(){
+    int result = calculateTimeBetween(4, 15, 5, 15);
+    Assertions.assertEquals(30, result);
+  }
+
   @Test
   public void test_Day1_LT_Day2_Month1_EQ_Month2(){
-
+    int result = calculateTimeBetween(5, 5, 5, 6);
+    Assertions.assertEquals(1, result);
   }
 
   @Test
   public void test_Day1_LT_Day2_Month1_NEQ_Month2(){
-
+    int result = calculateTimeBetween(5, 5, 6, 6);
+    Assertions.assertEquals(32, result);
   }
 
   @Test
   public void test_Day2_LT_Day1_Month1_EQ_Month2(){
-
+    int result = calculateTimeBetween(4, 6, 4, 3);
+    Assertions.assertEquals(362, result);
   }
 
   @Test
   public void test_Day2_LT_Day1_Month1_NEQ_Month2(){
-
+    int result = calculateTimeBetween(4, 6, 5, 3);
+    Assertions.assertEquals(27, result);
   }
 
+
+  /**
+   * Added these two to test for months.
+   */
+  @Test
+  public void test_Day2_EQ_Day1_Month1_GT_Month2(){
+    int result = calculateTimeBetween(5, 6, 3, 6);
+    Assertions.assertEquals(304, result);
+  }
+
+  @Test
+  public void test_Day2_NEQ_Day1_Month1_GT_Month2(){
+    int result = calculateTimeBetween(5, 6, 3, 3);
+    Assertions.assertEquals(301, result);
+  }
   /** INVALID INPUTS **/
 
   @Test
   public void test_Month_LT_31_days_Day_EQ_31(){
-    // Expect throw error
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      DatesImproved.calculateTimeBetween(2, 31, 5, 15);
+    });
   }
 
   @Test
   public void test_Month_GT_12(){
-    // Expect throw error
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      DatesImproved.calculateTimeBetween(13, 31, 5, 15);
+    });
   }
 
   @Test
   public void test_Month_LT_1(){
-    // Expect throw error
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      DatesImproved.calculateTimeBetween(-1, 31, 5, 15);
+    });
   }
 
   @Test
   public void test_Day_GT_31(){
-    // Expect throw error
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      DatesImproved.calculateTimeBetween(2, 60, 5, 15);
+    });
   }
 
 }
